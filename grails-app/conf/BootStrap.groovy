ab1b8a0548e072c46e06c63d1ebf10b7599974e4
Ankur Tripathi <ankur@intelligrape.com>
2013-02-04 00:53:30 +0530

    Association binding

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,19 @@
+import bootcamp.demo.Author
+
 class BootStrap {
 
     def init = { servletContext ->
+        createSampleAuthors()
     }
+
+
     def destroy = {
+    }
+
+    void createSampleAuthors() {
+        if (!Author.count()) {
+            new Author(name: "Bond").save()
+            new Author(name: "Batman").save()
+        }
     }
 }
--- grails-app/controllers/bootcamp/demo/BookController.groovy
+++ grails-app/controllers/bootcamp/demo/BookController.groovy
@@ -37,7 +37,7 @@ class BookController {
         redirect action: 'list'
     }
 
-    //http://localhost:8080/Controllers-II/book/paramConversionMethods?age=26&names=ankur&names=tripathi&dob=3-12-2012
+    //http://localhost:8080/Controllers-II/book/paramConversionMethods?age=10&names=obama&names=osama&dob=3-12-2012
     def paramConversionMethods() {
         println "Data type of params.age : " + params.age?.class
         Integer age = params.int("age")
@@ -54,6 +54,13 @@ class BookController {
         render("Done !!!")
     }
 
+    //http://localhost:8080/Controllers-II/book/bindParamsToArguments?age=10&name=salman
+    def bindParamsToArguments(Integer age, String name) {
+        println "Age: ${age}"
+        println "Name : ${name}"
+        render("Age $age - Name $name")
+    }
+
     //http://localhost:8080/Controllers-II/book/dateBinding?title=grails-date&publishedDate=2-12-2012
     //Error demo as well removing bindData and binding with implicit constructor, message.properties date format ?
     def dateBinding() {
@@ -63,6 +70,13 @@ class BookController {
         book.publishedDate = params.date('publishedDate', 'dd-MM-yyyy')
         book.save()
         redirect action: 'list'
+    }
+
+    //http://localhost:8080/Controllers-II/book/addAuthor?title=grails-date&author.id=1
+    def addAuthor() {
+        Book book = new Book(params)
+        book.save()
+        redirect(action: 'list')
     }
 
 }
